5a4db2c70aae4805aa3006efed81f90cf08ca4be
unknown <tankwoo@icloud.com>
2022-12-28 14:08:40 +0800

    fix

--- AgentHelper.py
+++ AgentHelper.py
@@ -37,7 +37,7 @@ class AgentHelper(object):
         command['config'] = {}
         self.req.send_string(json.dumps(command))
         result = self.req.recv_string()
-        json_result = json.loads(result)
+        json_result = json.loads(str(result))
         logging.debug("Deploy monitor slave on [{}] result: {}".format(self.ip, json_result))
         if not json_result['success']:
             raise Exception("部署从属机监控服务失败!")
